68526147826a64513483388015b9851cc1340d22
Kinuseka <66762783+Kinuseka@users.noreply.github.com>
2022-04-03 16:22:37 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,8 +9,8 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @require	     https://cdn.jsdelivr.net/npm/toastify-js
 // @resource     TOASTIFY_CSS https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css
-// @updateURL    https://github.com/Kinuseka/NyanCatrPlace/raw/main/Bot/bot.js
-// @downloadURL  https://github.com/Kinuseka/NyanCatrPlace/raw/main/Bot/bot.js
+// @updateURL    https://github.com/Kinuseka/Bot/raw/master/bot.js
+// @downloadURL  https://github.com/Kinuseka/Bot/raw/master/bot.js
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // ==/UserScript==
